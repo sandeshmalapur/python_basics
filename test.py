c79347a8d53d7e3817689c20cdba70dc196d4604
Sandesh Malapur <malapursandesh@gmail.com>
2026-06-25 16:53:44 +0530

    git commands

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("Hello World")
+# for _ in range(3):
+#     print("Hello World","world",sep="#")
+# print("abc">"abd")
+# x=234
+# print(type(x))
+# print("""my name is 
+# ironman 
+# thanku""")
+# x='hello'
+# print(x[0])
+for i in range(10):
+    if i%2==0:
+        print(i)
